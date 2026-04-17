205920525cebbf3b4c3e606fba5e9ae39fb5cdef
dart7272 <dar.t31012002@gmail.com>
2026-04-17 10:51:59 -0400

    se limpio el codigo y se resolvio el problema de los numeros primos

--- prueba_python.py
+++ prueba_python.py
@@ -1,22 +1,33 @@
 def primo(num):
-    if num%2==0:
+    if num == 2:
+        return True
+    elif num%2==0:
         return False
     else:
-        cont=0
-        for aux in range(1,9):
+        cont = 0
+        a = int(num**0.5)
+        for aux in range(1,a+1):
             if num%aux==0:
-                cont +1
-        if cont >=3:
+                cont += 1
+        if cont > 1:
             return False
         else:
             return True
 print ("esta funcion toma un rango de numeros y muestra todos los \n numeros primos contenidos en ese rango")
 a = int(input("introduce el inicio del rango: "))
 b = int(input("introduce el final del rango: "))
-print (f"los numeros primos que hay entre {a} y {b} son: ")
-for i in range(a, b):
-	if primo(i + 1):
-			print(i + 1, end=", ")
-print()
+condicion = False
+while condicion == False:
+    if a < b:
+        condicion = True
+        print (f"los numeros primos que hay entre {a} y {b} son: ")
+        for i in range(a, b):
+            if primo(i + 1):
+                    print(i + 1, end=", ")
+    else:
+        print(f"\n---------------------------\nel numero final debe ser mayor que el inicial\n-----------------------------")
+        a = int(input("introduce el inicio del rango: "))
+        b = int(input("introduce el final del rango: "))
+
 
 
